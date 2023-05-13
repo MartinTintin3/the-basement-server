0edaf88b860a27f221fe8cbad278ef40e1ebc3e8
MartinTintin3 <maroymart@gmail.com>
2023-05-13 16:00:42 -0400

    e

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-import express from "express";
-const app = express();
 import http from "http";
 
 import Server from "./server.js";
 
-const server = new Server(http.createServer(app), ":memory:");
-
-app.use(express.static("client"));
+const server = new Server(http.createServer(), ":memory:");
 
 server.http_server.listen(3000, () => {
 	console.log("listening on *:3000");
